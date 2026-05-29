70612d2bda7051c012a3d593927f49bbadaa32b6
Vinity12 <ashraf12rama@gmail.com>
2026-05-29 21:26:14 +0700

    Delete interaction.js

--- interaction.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector('.element');
-let clickCount = 0;
-
-function addCounter() {
-    if (clickCount < 10) {
-        clickCount++;
-        btn.innerHTML = `${clickCount.toString()}/10`;
-    }
-}
